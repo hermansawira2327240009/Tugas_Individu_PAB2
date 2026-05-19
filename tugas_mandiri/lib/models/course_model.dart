e2db92e991b6f4ee66483e841fb94dac94504e15
HermanS <hermansawira_2327240009@mhs.mdp.ac.id>
2026-05-19 10:08:19 +0700

    perbaikan

--- tugas_mandiri/lib/models/course_model.dart
+++ tugas_mandiri/lib/models/course_model.dart
@@ -28,4 +28,13 @@ class CourseModel {
 
   @override
   String toString() => name;
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+    return other is CourseModel && other.id == id;
+  }
+
+  @override
+  int get hashCode => id.hashCode;
 }
